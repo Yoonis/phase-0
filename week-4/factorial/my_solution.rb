5cffdc9eef4408f178c6d1195f0dd5b6ec79c496
Eunice Do <eunicedx@gmail.com>
2015-10-02 12:42:49 -0400

    4.7.1 final with pseudo

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -2,6 +2,22 @@
 
 # I worked on this challenge [with: Joshua Abrams].
 
+=begin Pseudocode
+
+Input: (num), where num is a positive integer
+Output: "brain", the variable result of a for loop
+
+define method factorial
+initialize variable "brain" to = 1
+initialize variable "melting" in the for loop
+	for all numbers "melting" in range 2 through num inclusive, do
+		brain = brain*melting
+	endfor
+return brain
+end method
+
+=end
+
 # Your Solution Below
 
 def factorial(num)
@@ -11,5 +27,3 @@ def factorial(num)
 	end
 	return brain
 end
-
-puts factorial(5)
